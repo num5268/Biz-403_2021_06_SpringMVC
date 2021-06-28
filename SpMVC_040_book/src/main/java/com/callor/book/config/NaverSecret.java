cfe335da42e736b6d6161a3e091cf4b78f1ba772
num5268 <num5268@gmail.com>
2021-06-28 15:13:41 +0900

    num5268

--- SpMVC_040_book/src/main/java/com/callor/book/config/NaverSecret.java
+++
@@ -1,8 +0,0 @@
-package com.callor.book.config;
-
-public class NaverSecret {
-
-	
-	public static final String NAVER_CLIENT_ID="asdasd";
-	public static final String NAVER_CLIENT_SECRET="asdasd";
-}
